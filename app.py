e42f3d7f6e5ce3a27d34c53bd9470e0b699667b5
Sridhar5879 <45038068+Sridhar5879@users.noreply.github.com>
2019-12-02 01:20:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -104,7 +104,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def order_status():
-  global userid_global
   if request.method == 'POST':
       #Taking the input query from user and converting it to an usable string
       try:
@@ -123,45 +122,40 @@ def order_status():
           aa['sss'] = userid
           #aa['result'] = aa['sss']
           aa['result'] = "You have successfully logged in! How can I help you?"
-          userid_global = userid
           return aa
       except ValueError:
-          if userid_global != "":
-              query = request.form.get('ui_query')
-              query1 = pd.Series(query)
-              query2 = query1.apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
-              query2=  [w for w in query2 if not w in stop]
-            
-              # Converting query2 into a string
-              k=convert(query2)
-              #Implementation fuzzywuzzy algorithm to find the closest match
-              from fuzzywuzzy import process
-              ## To Get Related questions based on ratio
-              choices_dict = {idx: el for idx, el in enumerate(dfq_1)}
-              Ratios = process.extract(k,choices_dict,limit=3)
-              ChatReply=(tuple(Ratios[0]))
-              j=(tuple(Ratios[1]))
-              l=(tuple(Ratios[2]))
-              if ChatReply[1]<70:
-                  aa = {}
-                  aa['input'] = request.form.get('ui_query')
-                  aa['result'] = "Sorry but your query did not match with any of our records, please try with another query"
+          query = request.form.get('ui_query')
+          query1 = pd.Series(query)
+          query2 = query1.apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+          query2=  [w for w in query2 if not w in stop]
+          
+          # Converting query2 into a string
+          k=convert(query2)
+          #Implementation fuzzywuzzy algorithm to find the closest match
+          from fuzzywuzzy import process
+          ## To Get Related questions based on ratio
+          choices_dict = {idx: el for idx, el in enumerate(dfq_1)}
+          Ratios = process.extract(k,choices_dict,limit=3)
+          ChatReply=(tuple(Ratios[0]))
+          j=(tuple(Ratios[1]))
+          l=(tuple(Ratios[2]))
+          if ChatReply[1]<70:
+              aa = {}
+              aa['input'] = request.form.get('ui_query')
+              aa['result'] = "Sorry but your query did not match with any of our records, please try with another query"
                   
-              elif ChatReply[1]>=70 and ChatReply[2] <= 48:
-                  aa = {}
-                  aa['input'] = request.form.get('ui_query')
-                  aa['result'] = dfa[ChatReply[2]]
+          elif ChatReply[1]>=70 and ChatReply[2] <= 48:
+              aa = {}
+              aa['input'] = request.form.get('ui_query')
+              aa['result'] = dfa[ChatReply[2]]
                   
-              else:
-                  aa = {}
-                  aa['input'] = request.form.get('ui_query')
-                  aa['result'] = dfa[ChatReply[2]]
-                  print('\n','NEAREST MATCH','\n''\n',dfq[ChatReply[2]],'\n',dfa[ChatReply[2]],'\n','\n','DO YOU MEAN?','\n',dfq[j[2]],'\n',dfq[l[2]])
-              return aa
           else:
               aa = {}
-              aa['result'] = "You have not entered correct userid. Please try again."
-              return aa
+              aa['input'] = request.form.get('ui_query')
+              aa['result'] = dfa[ChatReply[2]]
+              print('\n','NEAREST MATCH','\n''\n',dfq[ChatReply[2]],'\n',dfa[ChatReply[2]],'\n','\n','DO YOU MEAN?','\n',dfq[j[2]],'\n',dfq[l[2]])
+          return aa
+      
   else:
       return render_template('chat.html')
 
