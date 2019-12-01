4939dd130e1fd00967abfbecda7d8e71aba8db90
Sridhar5879 <45038068+Sridhar5879@users.noreply.github.com>
2019-12-01 22:59:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def order_status():
       aa = {}
       aa['input'] = int(request.form.get('ui_query'))
       aa['sss'] = userid
-      aa['result'] = request.form.get('user_id')
+      aa['result'] = request.form.get('sss')
       #aa['result'] = "You have successfully logged in! How can I help you?"
       return aa
       query1 = pd.Series(query)
